0252c03a827e30e89748f7fb46fe378f9eaff819
stoltzmaniac <scott@stoltzmanconsulting.com>
2019-01-09 05:46:48 -0700

    Add mongo notifier

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,14 @@ done
 
 echo "PostgreSQL started"
 
+
+echo "Waiting for mongo..."
+
+while ! nc -z web-mongo 27017; do
+  sleep 0.1
+done
+
+echo "MongoDB started"
+
+
 python manage.py run -h 0.0.0.0
