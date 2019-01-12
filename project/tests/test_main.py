b95d3fb2415bb932199f7c0536d208639cab6752
stoltzmaniac <scott@stoltzmanconsulting.com>
2019-01-12 07:55:10 -0700

    Add tests to travis

--- project/tests/test_main.py
+++ project/tests/test_main.py
@@ -11,14 +11,14 @@ class TestMainBlueprint(BaseTestCase):
         # Ensure Flask is setup.
         response = self.client.get("/", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Welcome!", response.data)
+        self.assertIn(b"Fort Collins Data Science Portal", response.data)
         self.assertIn(b"Login", response.data)
 
     def test_about(self):
         # Ensure about route behaves correctly.
         response = self.client.get("/about", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"About", response.data)
+        self.assertIn(b"Meetup", response.data)
 
     def test_404(self):
         # Ensure 404 error is handled.
