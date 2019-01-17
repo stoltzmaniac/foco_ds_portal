3d146f0b31e252a07f2a3e4beca5567a524482c4
stoltzmaniac <scott@stoltzmanconsulting.com>
2019-01-17 15:06:33 -0700

    Fixed start of container

--- project/twitter_stream/stream_listener.py
+++ project/twitter_stream/stream_listener.py
@@ -1,11 +1,18 @@
 import os
 import tweepy
+from pymongo import MongoClient
 
 
 TWTR_CONSUMER_KEY = os.getenv("TWTR_CONSUMER_KEY")
 TWTR_CONSUMER_SECRET = os.getenv("TWTR_CONSUMER_SECRET")
 TWTR_TOKEN_KEY = os.getenv("TWTR_TOKEN_KEY")
 TWTR_TOKEN_SECRET = os.getenv("TWTR_TOKEN_SECRET")
+MONGO_URI = os.getenv("MONGO_URI")
+
+
+client = MongoClient(MONGO_URI)
+db = client.get_database()
+twitter_collection = db.twitter_stream
 
 
 auth = tweepy.OAuthHandler(TWTR_CONSUMER_KEY, TWTR_CONSUMER_SECRET)
@@ -16,7 +23,8 @@ api = tweepy.API(auth)
 class TweetListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        print(status.text)
+        # print(status.text)
+        pass
 
     def on_error(self, status_code):
         if status_code == 420:
@@ -25,3 +33,6 @@ class TweetListener(tweepy.StreamListener):
 
 stream = tweepy.Stream(auth=api.auth, listener=TweetListener())
 stream.filter(track=["rstats"])
+
+
+
